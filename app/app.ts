29b88d331ad066ec970815ffbd6d9f5eb083feb7
Shawn Lauzon <shawn.lauzon@gmail.com>
2016-06-29 19:29:59 -0400

    Initial import of generated files

---
+++ app/app.ts
@@ -0,0 +1,44 @@
+import { Component, ViewChild } from '@angular/core';
+import { App, ionicBootstrap, Platform, Nav } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+
+import { UserProfile } from './pages/page1/page1';
+import { Programs } from './pages/programs/programs';
+
+@Component({
+  templateUrl: 'build/app.html'
+})
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = Programs;
+
+  pages: Array<{title: string, component: any}>
+
+  constructor(private platform: Platform) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'John Smith', component: UserProfile },
+      { title: 'Programs', component: Programs }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
+
+ionicBootstrap(MyApp);
---
+++ app/pages/page1/page1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/page1/page1.html'
+})
+export class UserProfile {
+
+  constructor(private navController: NavController) {
+  }
+}
